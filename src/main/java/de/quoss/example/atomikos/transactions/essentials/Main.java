1c6d538e4d64cafff15f816501755fb12e8e1980
Clemens Quoss <clemens@quoss.de>
2022-01-15 16:40:19 +0100

    Share current progress

--- src/main/java/de/quoss/example/atomikos/transactions/essentials/Main.java
+++ src/main/java/de/quoss/example/atomikos/transactions/essentials/Main.java
@@ -2,10 +2,18 @@ package de.quoss.example.atomikos.transactions.essentials;
 
 import com.atomikos.icatch.jta.UserTransactionManager;
 import com.atomikos.jdbc.AtomikosDataSourceBean;
+import com.atomikos.jms.AtomikosConnectionFactoryBean;
+import org.apache.activemq.artemis.jms.client.ActiveMQXAConnectionFactory;
 import org.h2.jdbcx.JdbcDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
 import javax.sql.DataSource;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
@@ -13,6 +21,7 @@ import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.Status;
 import javax.transaction.SystemException;
+import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -22,7 +31,7 @@ public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    private void run() throws HeuristicMixedException, HeuristicRollbackException, RollbackException, NotSupportedException, SQLException, SystemException {
+    private void run() throws HeuristicMixedException, HeuristicRollbackException, JMSException, RollbackException, NotSupportedException, SQLException, SystemException {
 
         // Set up h2 xa data source
         JdbcDataSource dataSource = new JdbcDataSource();
@@ -30,41 +39,75 @@ public class Main {
         dataSource.setPassword("");
         dataSource.setURL("jdbc:h2:~/test");
 
+        // set up active mq artemis xa connection factory
+        ActiveMQXAConnectionFactory acf = new ActiveMQXAConnectionFactory();
+
         // Atomikos implementations
         UserTransactionManager utm = new UserTransactionManager();
         AtomikosDataSourceBean adsb = new AtomikosDataSourceBean();
         adsb.setXaDataSource(dataSource);
         adsb.setUniqueResourceName("atomikosJdbc");
+        AtomikosConnectionFactoryBean acfb = new AtomikosConnectionFactoryBean();
+        acfb.setXaConnectionFactory(acf);
+        acfb.setUniqueResourceName("atomikosJms");
 
         // Standard interfaces
         TransactionManager tm = utm;
         DataSource ds = adsb;
+        ConnectionFactory cf = acfb;
 
         // init
         utm.init();
 
-        // begin transaction
+        // begin transaction, timout 15 minutes
+        tm.setTransactionTimeout(15 * 60);
         tm.begin();
 
-        // do something on database
-        try (Connection connection = ds.getConnection();
-             Statement statement = connection.createStatement()) {
+        // receive from jms (to-queue) and insert into database (customer table)
+        try (Connection dbConnection = ds.getConnection();
+             Statement statement = dbConnection.createStatement();
+             javax.jms.Connection jmsConnection = cf.createConnection();
+             Session session = jmsConnection.createSession();
+             MessageConsumer consumer = session.createConsumer(session.createQueue("to-queue"))) {
+
             // drop table if exists
-            String sql = "drop table customer if exists";
+            String sql = "drop table messages if exists";
             statement.execute(sql);
             // create table
-            sql = "create table customer( id integer primary key, name varchar(50) )";
+            sql = "create table messages( id integer primary key, message_id varchar(50) )";
             statement.execute(sql);
-            // insert customer zero
-            sql = "insert into customer values ( 0, 'Clemens Quoß' )";
-            statement.execute(sql);
-        } catch (SQLException e) {
+
+            // consume all messages, 10 minute timeout
+            Message message = consumer.receive(10 * 60 * 1000L);
+            while (message != null) {
+
+                // get id and message id from jms
+                String messsageId = message.getJMSMessageID();
+                int id = fetchIdFromBody(message.getBody(Object.class));
+
+                // insert message
+                sql = String.format("insert into customer values ( %s, '%s' )", id, messsageId);
+                statement.execute(sql);
+
+                // fetch next message
+                message = consumer.receive(10 * 60 * 1000L);
+
+            }
+
+        } catch (JMSException | SQLException e) {
+
+            // roll back user transaction on error
             tm.rollback();
             throw e;
+
         } finally {
-            if (tm.getTransaction().getStatus() == Status.STATUS_ACTIVE) {
+
+            // commit transaction if still active
+            Transaction tx = tm.getTransaction();
+            if (tx != null && tx.getStatus() == Status.STATUS_ACTIVE) {
                 tm.commit();
             }
+
         }
 
         // shut down
@@ -73,10 +116,20 @@ public class Main {
 
     }
 
+    private int fetchIdFromBody(final Object o) {
+        int result = 0;
+        try {
+            result = Integer.parseInt("" + o);
+        } catch (NumberFormatException e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
+        }
+        return result;
+    }
+
     public static void main(final String[] args) {
         try {
             new Main().run();
-        } catch (HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SQLException | SystemException e) {
+        } catch (HeuristicMixedException | HeuristicRollbackException | JMSException | NotSupportedException | RollbackException | SQLException | SystemException e) {
             LOGGER.error(e.getLocalizedMessage(), e);
             System.exit(1);
         }
